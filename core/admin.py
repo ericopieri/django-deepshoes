bc3f87e33290ddfd5bd7e4875be6a5fc6f4e209c
Erico Pieri <erico.pieri@gmail.com>
2022-09-04 18:45:52 -0300

    registro na admin page de todas as models criadas

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from core.models import Tipo_Usuario, Usuario, Endereco
+from core.models import Tipo_Usuario, Usuario, Endereco, Cartao, Cor, Tamanho, Marca, Produto, Forma_Pagamento, Pedido, Itens_Pedido, Avaliacao
 
 
 admin.site.register(Tipo_Usuario)
 admin.site.register(Usuario)
 admin.site.register(Endereco)
+admin.site.register(Cartao)
+admin.site.register(Cor)
+admin.site.register(Tamanho)
+admin.site.register(Marca)
+admin.site.register(Produto)
+admin.site.register(Forma_Pagamento)
+admin.site.register(Pedido)
+admin.site.register(Itens_Pedido)
+admin.site.register(Avaliacao)
+

e1a34c96621075a8dbe162f3e4246642ded8d8f9
ericopieri <erico.pieri@gmail.com>
2022-09-07 16:58:54 -0300

    registro de todas as views feitas até aqui

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,21 +1,22 @@
-"""projeto URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from core.views import EnderecoViewSet, CartaoViewSet, CorViewSet, TamanhoViewSet, MarcaViewSet, Forma_PagamentoViewSet, PedidoViewSet, AvaliacaoViewSet
+
+router = DefaultRouter()
+router.register(r'enderecos', EnderecoViewSet)
+router.register(r'cartoes', CartaoViewSet)
+router.register(r'cores', CorViewSet)
+router.register(r'forma_pagamento', Forma_PagamentoViewSet)
+router.register(r'marcas', MarcaViewSet)
+router.register(r'tamanhos', TamanhoViewSet)
+router.register(r'peidos', PedidoViewSet)
+router.register(r'avaliacoes', AvaliacaoViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]

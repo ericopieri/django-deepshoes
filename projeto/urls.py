bc123a9c52bb33fa6b79bf286322f590f6b2a934
vagnercardozo <vagnercardozo2002@gmail.com>
2022-09-08 16:09:30 -0300

    registrado view para Produto

--- projeto/urls.py
+++ projeto/urls.py
@@ -4,15 +4,16 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from core.views import (
+    UsuarioViewSet,
     EnderecoViewSet,
     CartaoViewSet,
     CorViewSet,
     TamanhoViewSet,
     MarcaViewSet,
     Forma_PagamentoViewSet,
+    ProdutoViewSet,
     PedidoViewSet,
-    AvaliacaoViewSet,
-    UsuarioViewSet,
+    AvaliacaoViewSet
 )
 
 router = DefaultRouter()
@@ -21,6 +22,7 @@ router.register(r"enderecos", EnderecoViewSet)
 router.register(r"cartoes", CartaoViewSet)
 router.register(r"cores", CorViewSet)
 router.register(r"forma_pagamento", Forma_PagamentoViewSet)
+router.register(r"produtos", ProdutoViewSet)
 router.register(r"marcas", MarcaViewSet)
 router.register(r"tamanhos", TamanhoViewSet)
 router.register(r"pedidos", PedidoViewSet)

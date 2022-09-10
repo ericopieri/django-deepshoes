ed2c93a9144c45a4631eff8d04a747a2941b3aea
ericopieri <erico.pieri@gmail.com>
2022-09-09 21:09:55 -0300

    correção virgula faltando no settings

--- projeto/settings.py
+++ projeto/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication"
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
 }
 

489fd0285121eac8a717dc913d805b1aa860ddc4
ericopieri <erico.pieri@gmail.com>
2022-09-12 12:52:52 -0300

    prorrogando o tem de expiração do token e refresh

--- projeto/settings.py
+++ projeto/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from datetime import timedelta
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,3 +123,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=180),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}

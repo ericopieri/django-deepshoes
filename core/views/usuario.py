2f1d4b90cc7ae693a04d879adbb7fe5d67126e84
ericopieri <erico.pieri@gmail.com>
2022-09-17 17:42:58 -0300

    atualizando as permissions pra cadastro de usuario

--- core/views/usuario.py
+++ core/views/usuario.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from core.models import Usuario
 from core.serializers import UsuarioSerializer
@@ -7,3 +8,8 @@ from core.serializers import UsuarioSerializer
 class UsuarioViewSet(ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [AllowAny()]
+        return [IsAuthenticated()]

845628346bcf7aac60b8871fdb94102b62ac0486
pjgplantas <pjgplantas@gmail.com>
2022-10-03 15:13:15 -0300

    pagination em produtos

--- core/views/produto.py
+++ core/views/produto.py
@@ -3,7 +3,10 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import Produto
 from core.serializers import ProdutoSerializer
 
+from core.paginations.produto import ProdutoPagination
+
 
 class ProdutoViewSet(ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
+    pagination_class = ProdutoPagination

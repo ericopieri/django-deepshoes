6ad798b87e40e7affc441892b924ab8545ad2387
Erico Pieri <erico.pieri@gmail.com>
2022-10-12 21:33:04 -0300

    conectando postgres railway

--- projeto/settings.py
+++ projeto/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "projeto.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("PGDATABASE"),
+        "USER": config("PGUSER"),
+        "PASSWORD": config("PGPASSWORD"),
+        "HOST": config("PGHOST"),
+        "PORT": config("PGPORT"),
     }
 }
 

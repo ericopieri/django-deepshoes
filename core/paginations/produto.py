8d160ab0bfa384203b09bdfda24a0707d4455d3c
Erico Pieri <erico.pieri@gmail.com>
2022-10-15 00:58:04 -0300

    100 limit pagination produto

--- core/paginations/produto.py
+++ core/paginations/produto.py
@@ -4,4 +4,4 @@ from rest_framework.pagination import PageNumberPagination
 class ProdutoPagination(PageNumberPagination):
     page_size = 8
     page_size_query_param = "page_size"
-    max_page_size = 8
+    max_page_size = 100

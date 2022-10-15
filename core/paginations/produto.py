b35b6e24c020e74c05bd34f80cc56854793ad1e4
Erico Pieri <erico.pieri@gmail.com>
2022-10-14 23:22:58 -0300

    voltando para 8 tamanho das paginas

--- core/paginations/produto.py
+++ core/paginations/produto.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class ProdutoPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 8
     page_size_query_param = "page_size"
-    max_page_size = 2
+    max_page_size = 8

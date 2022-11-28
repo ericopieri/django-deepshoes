55e6066465cb8291414dab04c4c2c0a9095fe9dc
ericopieri <erico.pieri@gmail.com>
2022-11-27 23:45:36 -0300

    retornando id para o front

--- core/serializers/endereco.py
+++ core/serializers/endereco.py
@@ -14,6 +14,7 @@ class EnderecoSerializer(ModelSerializer):
             "numero",
             "complemento",
             "referencia",
+            "id",
         )
 
     def create(self, validated_data):
